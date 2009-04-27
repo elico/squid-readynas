014f09e3461a8833eaead82533941e09898b6872
Mark Nottingham <mnot@mnot.net>
2009-04-27 12:50:59 +1000

    Remove extraneous files fom /etc/frontview

--- addons_sdk/SQUID/files/etc/frontview/addons/bin/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#
-# This should contain necessary code to start the service
-
-start-stop-daemon -S -b -m --pidfile /var/run/SQUID.pid -q -x /etc/frontview/addons/bin/SQUID/SQUID_service
